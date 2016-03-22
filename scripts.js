bac56cf0591ec1ca275ce17a25684e51c1249966
Greg <gsc229@gmail.com>
2016-03-22 04:34:05 -0600

    Slower scroll
    went from 600 to 1200 milliseconds on full page scroll

--- scripts.js
+++ scripts.js
@@ -2,25 +2,25 @@ $(document).ready(function(){
     
   $(".design-goals").click(function() {
             $("html,body").animate(
-                    {scrollTop:$("#design-goals-periscope").offset().top-85}, 600
+                    {scrollTop:$("#design-goals-periscope").offset().top-85}, 1200
             )
         });
     
      $(".social-media").click(function() {
             $("html,body").animate(
-                    {scrollTop:$("#periscope-text").offset().top-85}, 600
+                    {scrollTop:$("#periscope-text").offset().top-85}, 1200
             )
         });
     
      $(".navbar-brand").click(function() {
             $("html,body").animate(
-                    {scrollTop:$("body").offset().top}, 600
+                    {scrollTop:$("body").offset().top}, 1200
             )
         });
     
      $(".top").click(function() {
             $("html,body").animate(
-                    {scrollTop:$("body").offset().top}, 600
+                    {scrollTop:$("body").offset().top}, 1200
             )
         });
 
